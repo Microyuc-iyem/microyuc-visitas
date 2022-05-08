77d832b1ece59a6a12ba40486a8a7eac4c28c6b1
Sigfredo Olmedo <sigfredoolmedo6c17@gmail.com>
2022-05-08 02:23:55 -0500

    Add new inputs to the form for letter generator

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php
+
+include ('./config/db_connect.php');
+
+// Write query for all acreditados
+$sql = 'SELECT nombre, folio, municipio, email, direccion_particular FROM acreditado ORDER BY nombre';
+
+// make query and & get result
+$result = mysqli_query($conn, $sql);
+
+// Fetch the resulting rows as an array
+$acreditados = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// Free result from memory
+mysqli_free_result($result);
+
+// Close connection
+mysqli_close($conn);
+
+?>
+
 <!doctype html>
 <html lang="es-MX">
 <head>
@@ -5,14 +26,22 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/main.css">
     <title>Microyuc | Generador de Carta</title>
 </head>
 <body>
 <header>
-<nav>
-
-</nav>
+    <nav>
+        <h4 class="text-6xl">Acreditados</h4>
+        <div>
+            <?php foreach ($acreditados as $acreditado) { ?>
+            <p>
+                <?php echo htmlspecialchars($acreditado['nombre'])?>
+                <?php echo htmlspecialchars($acreditado['folio'])?>
+            </p>
+            <?php } ?>
+        </div>
+    </nav>
 </header>
 <div class="container">
     <div class="letterGeneratorForm__container">
@@ -20,11 +49,13 @@
         <form class="letterGeneratorForm" action="./exportar.php" method="post">
             <div>
                 <label for="numero_expediente">Número de expediente<span class="asterisk">*</span>:</label>
-                <input class="letterGeneratorForm__input" type="text" id="numero_expediente" name="numero_expediente" required>
+                <input class="letterGeneratorForm__input" type="text" id="numero_expediente"
+                       name="numero_expediente" required>
             </div>
             <div>
                 <label for="nombre_cliente">Nombre del cliente<span class="asterisk">*</span>: </label>
-                <input class="letterGeneratorForm__input" type="text" id="nombre_cliente" name="nombre_cliente" required>
+                <input class="letterGeneratorForm__input" type="text" id="nombre_cliente" name="nombre_cliente"
+                       required>
             </div>
             <div>
                 <label for="calle">Calle: </label>
@@ -36,7 +67,8 @@
             </div>
             <div>
                 <label for="numero_direccion">Número: </label>
-                <input class="letterGeneratorForm__input" type="number" id="numero_direccion" name="numero_direccion">
+                <input class="letterGeneratorForm__input" type="number" id="numero_direccion"
+                       name="numero_direccion">
             </div>
             <div>
                 <label for="colonia_fraccionamiento">Colonia/fraccionamiento: </label>
@@ -56,26 +88,51 @@
                 <input class="letterGeneratorForm__input" type="date" id="fecha_firma" name="fecha_firma" required>
             </div>
             <div>
+                <label for="documentacion">Documentación: </label>
+                <input class="letterGeneratorForm__input" type="text" id="documentacion" name="documentacion">
+            </div>
+            <div>
+                <label for="comprobacion_monto">Monto de comprobación: </label>
+                <input class="letterGeneratorForm__input" type="number" id="comprobacion_monto" name="comprobacion_monto">
+            </div>
+            <div>
+                <label for="comprobacion_tipo">Tipo de comprobación: </label>
+                <input class="letterGeneratorForm__input" type="text" id="comprobacion_tipo" name="comprobacion_tipo">
+            </div>
+            <div>
+                <label for="pagos_fecha_inicial">Fecha inicial<span class="asterisk">*</span>: </label>
+                <input class="letterGeneratorForm__input" type="month" id="pagos_fecha_inicial" name="pagos_fecha_inicial" required>
+            </div>
+            <div>
+                <label for="pagos_fecha_final">Fecha final<span class="asterisk">*</span>: </label>
+                <input class="letterGeneratorForm__input" type="month" id="pagos_fecha_final" name="pagos_fecha_final" required>
+            </div>
+            <div>
                 <label for="tipo_credito">Tipo de crédito<span class="asterisk">*</span>: </label>
-                <input class="letterGeneratorForm__input" type="text" id="tipo_credito" name="tipo_credito" required>
+                <input class="letterGeneratorForm__input" type="text" id="tipo_credito" name="tipo_credito"
+                       required>
             </div>
             <div>
                 <label for="fecha_otorgamiento">Fecha de otorgamiento del crédito<span class="asterisk">*</span>:
                 </label>
-                <input class="letterGeneratorForm__input" type="date" id="fecha_otorgamiento" name="fecha_otorgamiento" required>
+                <input class="letterGeneratorForm__input" type="date" id="fecha_otorgamiento"
+                       name="fecha_otorgamiento" required>
             </div>
             <div>
                 <label for="monto_inicial">Monto inicial<span class="asterisk">*</span>: </label>
-                <input class="letterGeneratorForm__input" type="number" id="monto_inicial" name="monto_inicial" required>
+                <input class="letterGeneratorForm__input" type="number" id="monto_inicial" name="monto_inicial"
+                       required>
             </div>
             <div>
                 <label for="mensualidades_vencidas">Mensualidades vencidas<span class="asterisk">*</span>: </label>
-                <input class="letterGeneratorForm__input" type="number" id="mensualidades_vencidas" name="mensualidades_vencidas"
+                <input class="letterGeneratorForm__input" type="number" id="mensualidades_vencidas"
+                       name="mensualidades_vencidas"
                        required>
             </div>
             <div>
                 <label for="adeudo_total">Adeudo total<span class="asterisk">*</span>: </label>
-                <input class="letterGeneratorForm__input" type="number" id="adeudo_total" name="adeudo_total" required>
+                <input class="letterGeneratorForm__input" type="number" id="adeudo_total" name="adeudo_total"
+                       required>
             </div>
             <div class="letterGeneratorForm__btnContainer">
                 <input class="btn btn--animated" type="submit" value="Generar archivo">
@@ -85,5 +142,3 @@
 </div>
 </body>
 </html>
-
-<?php
