dd1b23c4126602f032fdf5283ddad19661249c81
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-05-30 13:56:47 -0600

    Add login.php file and css styles

---
+++ login.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="es_MX">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./dist/css/styles.css">
+    <title>Microyuc | Inicio de sesión</title>
+</head>
+<body style="">
+<div class="login">
+    <img src="./img/microyucfondo.png" alt="Logo de Microyuc" class="login__img">
+    <div class="login__container">
+        <h1 class="login__title">Iniciar sesión</h1>
+        <p class="login__subtitle">Introduce tus credenciales para iniciar sesión.</p>
+        <form action="" method="post" class="login__form">
+            <label for="user">
+                <input type="text" id="user" name="user" placeholder="Usuario" class="login__input" required>
+            </label>
+            <label for="password">
+                <input type="password" id="password" name="password" placeholder="Contraseña" class="login__input" required>
+            </label>
+            <input type="submit" value="Iniciar sesión" class="login__btn">
+        </form>
+    </div>
+</div>
+</body>
+</html>
