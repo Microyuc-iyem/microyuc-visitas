8ea9aa1463ad5546c726613ea2ea6795edf83bf8
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-06-02 21:27:38 -0600

    Add placeholder file for letter generator directory

--- index.php
+++ index.php
@@ -1,53 +1,53 @@
+<?php
+session_start();
+include('./config/db_connect.php');
+if (isset($_SESSION['login'])) {
+    header("Location: inicio.php");
+}
+
+$sql = 'SELECT nombre, password FROM usuario';
+
+$result = mysqli_query($conn, $sql);
+
+$usuarios = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+if ($_POST) {
+    if ($_POST['user'] == "Admin" && $_POST['password'] == "123456789@MY") {
+        $_SESSION['login'] = true;
+        header("Location: inicio.php");
+    } else {
+        echo "<h1 style='text-align: center'>Usuario o contraseña incorrectos</h1>";
+    }
+
+}
+
+?>
+
 <!doctype html>
-<html lang="es_MX" class="h-full bg-gray-50">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./css/main.css">
-    <title>Inicio de sesión</title>
+    <link rel="stylesheet" href="./dist/css/styles.css">
+    <title>Microyuc | Inicio de sesión</title>
 </head>
-<body class="h-full">
-<div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div class="max-w-md w-full space-y-8">
-        <div>
-            <img class="mx-auto h-242 w-auto" src="./img/microyucfondo.png"
-                 alt="Workflow">
-            <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Inicia sesión a tu cuenta</h2>
-        </div>
-        <form class="mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" value="true">
-            <div class="rounded-md shadow-sm -space-y-px">
-                <div>
-                    <label for="usuario" class="sr-only">Usuario</label>
-                    <input id="usuario" name="usuario" type="text" autocomplete="text" required
-                           class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                           placeholder="Nombre de usuario">
-                </div>
-                <div>
-                    <label for="password" class="sr-only">Contraseña</label>
-                    <input id="password" name="password" type="password" autocomplete="current-password" required
-                           class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                           placeholder="Contraseña">
-                </div>
-            </div>
-
-            <div>
-                <button type="submit"
-                        class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-          <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-            <!-- Heroicon name: solid/lock-closed -->
-            <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg"
-                 viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd"
-                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                    clip-rule="evenodd"/>
-            </svg>
-          </span>
-                    Iniciar sesión
-                </button>
-            </div>
+<body>
+<div class="login">
+    <img src="./img/microyucfondo.png" alt="Logo de Microyuc" class="login__img">
+    <div class="login__container">
+        <h1 class="login__title">Iniciar sesión</h1>
+        <p class="login__subtitle">Introduce tus credenciales para iniciar sesión.</p>
+        <form action="login.php" method="post" class="login__form">
+            <label for="user">
+                <input type="text" id="user" name="user" placeholder="Usuario" class="login__input" required>
+            </label>
+            <label for="password">
+                <input type="password" id="password" name="password" placeholder="Contraseña" class="login__input"
+                       required>
+            </label>
+            <input type="submit" value="Iniciar sesión" class="login__btn">
         </form>
     </div>
 </div>
