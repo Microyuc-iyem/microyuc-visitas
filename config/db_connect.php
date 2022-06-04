db10c123da90c2422dd354acae05b0ae937a68dd
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-06-03 23:00:49 -0600

    Change db connection for heroku

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,16 +1,16 @@
 <?php
 //Get Heroku ClearDB connection information
-/*$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
-$query_builder = TRUE;*/
+$query_builder = TRUE;
 
 // Connect to DB
-//$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-$conn = mysqli_connect('localhost', 'sig', '1234', 'microyuc_project');
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+//$conn = mysqli_connect('localhost', 'sig', '1234', 'microyuc_project');
 
 if (!$conn) {
     echo 'Connection error: ' . mysqli_connect_error();
