cb8b918cbb455f88d3bd3ef79f67ba52b422e417
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-07-15 16:16:59 -0600

    Migrate backend to a single file

--- generador-bitacora.php
+++ generador-bitacora.php
@@ -1,8 +1,220 @@
 <?php
-require_once './config/db_connect.php';
-session_start();
-if (!isset($_SESSION['login'])) {
-    header("Location: index.php");
+// Require database connection and PHPWord library
+require './config/db_connect.php';
+require './lib/phpword/vendor/autoload.php';
+require './includes/functions.php';
+
+check_login();
+
+$fmt = set_date_format_logbook();
+
+$bitacora = [
+    'acreditado_nombre' => '',
+    'acreditado_folio' => '',
+    'acreditado_municipio' => '',
+    'acreditado_garantia' => '',
+    'acreditado_telefono' => '',
+    'acreditado_email' => '',
+    'acreditado_direccion_negocio' => '',
+    'acreditado_direccion_particular' => '',
+    'aval_nombre' => '',
+    'aval_telefono' => '',
+    'aval_email' => '',
+    'aval_direccion' => '',
+    'gestion_fecha1' => '',
+    'gestion_via1' => '',
+    'gestion_comentarios1' => '',
+    'evidencia_fecha1' => '',
+    'evidencia_fotografia1' => '',
+];
+$errores = [
+    'acreditado_nombre' => '',
+    'acreditado_folio' => '',
+    'acreditado_municipio' => '',
+    'acreditado_garantia' => '',
+    'acreditado_telefono' => '',
+    'acreditado_email' => '',
+    'acreditado_direccion_negocio' => '',
+    'acreditado_direccion_particular' => '',
+    'gestion_fecha1' => '',
+    'gestion_via1' => '',
+    'evidencia_fotografia1' => '',
+];
+
+$tipos_gestion = ['Correo electrónico', 'Llamada telefónica', 'Visita'];
+
+$filtros = [];
+
+$movido = false;
+$ruta_subido = './uploads/';
+$tipos_permitidos = ['image/jpeg', 'image/png', 'image/gif', 'image/bmp', 'image/tiff', 'image/webp'];
+$exts_permitidas = ['jpeg', 'jpg', 'jpe', 'jif', 'jfif', 'png', 'gif', 'bmp', 'tif', 'tiff', 'webp'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // Setting filter settings
+    $filtros['acreditado_nombre']['filter'] = FILTER_VALIDATE_REGEXP;
+    $filtros['acreditado_nombre']['options']['regexp'] = '/^[A-zÀ-ÿ ]+$/';
+    $filtros['acreditado_folio']['filter'] = FILTER_VALIDATE_REGEXP;
+    $filtros['acreditado_folio']['options']['regexp'] = '/(^IYE{1,1})([\d\-]+$)/';
+    $filtros['acreditado_municipio']['filter'] = FILTER_VALIDATE_REGEXP;
+    $filtros['acreditado_municipio']['options']['regexp'] = '/[\s\S]+/';
+    $filtros['acreditado_garantia']['filter'] = FILTER_VALIDATE_REGEXP;
+    $filtros['acreditado_garantia']['options']['regexp'] = '/[\s\S]+/';
+    $filtros['acreditado_telefono']['filter'] = FILTER_VALIDATE_REGEXP;
+    $filtros['acreditado_telefono']['options']['regexp'] = '/^[\+\d\-\s\.\(\)]+$/';
+    $filtros['acreditado_email']['filter'] = FILTER_VALIDATE_EMAIL;
+    $filtros['acreditado_direccion_negocio']['filter'] = FILTER_VALIDATE_REGEXP;
+    $filtros['acreditado_direccion_negocio']['options']['regexp'] = '/[\s\S]+/';
+    $filtros['acreditado_direccion_particular']['filter'] = FILTER_VALIDATE_REGEXP;
+    $filtros['acreditado_direccion_particular']['options']['regexp'] = '/[\s\S]+/';
+    $filtros['aval_nombre']['filter'] = FILTER_VALIDATE_REGEXP;
+    $filtros['aval_nombre']['options']['regexp'] = '/[\s\S]+/';
+    $filtros['aval_nombre']['options']['default'] = '';
+    $filtros['aval_telefono']['filter'] = FILTER_VALIDATE_REGEXP;
+    $filtros['aval_telefono']['options']['regexp'] = '/[\s\S]+/';
+    $filtros['aval_telefono']['options']['default'] = '';
+    $filtros['aval_email']['filter'] = FILTER_VALIDATE_REGEXP;
+    $filtros['aval_email']['options']['regexp'] = '/[\s\S]+/';
+    $filtros['aval_email']['options']['default'] = '';
+    $filtros['aval_direccion']['filter'] = FILTER_VALIDATE_REGEXP;
+    $filtros['aval_direccion']['options']['regexp'] = '/[\s\S]+/';
+    $filtros['aval_direccion']['options']['default'] = '';
+    $filtros['gestion_fecha1']['filter'] = FILTER_VALIDATE_REGEXP;
+    $filtros['gestion_fecha1']['options']['regexp'] = '/^[\d\-]+$/';
+    $filtros['gestion_via1']['filter'] = FILTER_VALIDATE_REGEXP;
+    $filtros['gestion_via1']['options']['regexp'] = '/^(Correo electrónico|Llamada telefónica|Visita)+$/';
+    $filtros['gestion_comentarios1']['filter'] = FILTER_VALIDATE_REGEXP;
+    $filtros['gestion_comentarios1']['options']['regexp'] = '/[\s\S]+/';
+    $filtros['gestion_comentarios1']['options']['default'] = '';
+    $filtros['evidencia_fecha1']['filter'] = FILTER_VALIDATE_REGEXP;
+    $filtros['evidencia_fecha1']['options']['regexp'] = '/^[\d\-]+$/';
+    $filtros['evidencia_fecha1']['options']['default'] = '';
+
+    $bitacora = filter_input_array(INPUT_POST, $filtros);
+
+    $bitacora['evidencia_fotografia1'] = $_FILES['evidencia_fotografia1']['name'] ?? '';
+    $bitacora['evidencia_fecha1_texto'] = '';
+
+    if ($_FILES['evidencia_fotografia1']['error'] === 0) {
+        $tipo = mime_content_type($_FILES['evidencia_fotografia1']['tmp_name']);
+        $errores['evidencia_fotografia1'] = in_array($tipo, $tipos_permitidos) ? '' : 'Formato de archivo incorrecto. ';
+        $ext = strtolower(pathinfo($_FILES['evidencia_fotografia1']['name'], PATHINFO_EXTENSION));
+        $errores['evidencia_fotografia1'] .= in_array($ext, $exts_permitidas) ? '' : 'Extensión de archivo incorrecta.';
+
+        if (!$errores['evidencia_fotografia1']) {
+            $fotografia_nombre_archivo = create_filename($_FILES['evidencia_fotografia1']['name'], $ruta_subido);
+            $destino = $ruta_subido . $fotografia_nombre_archivo;
+            $movido = move_uploaded_file($_FILES['evidencia_fotografia1']['tmp_name'], $destino);
+        }
+    }
+
+    if ($movido) {
+        $bitacora['evidencia_fotografia1'] = $fotografia_nombre_archivo;
+        $bitacora['evidencia_fecha1'] = new DateTime($bitacora['evidencia_fecha1']);
+        $interval = new DateInterval('P1D');
+        $bitacora['evidencia_fecha1_texto'] = "Se visitó el negocio el " . datefmt_format($fmt, $bitacora['evidencia_fecha1']->add($interval)) . ".</w:t><w:br/><w:t>Fachada del negocio.";
+    }
+
+    // Error messages
+    $errores['acreditado_nombre'] = $bitacora['acreditado_nombre'] ? '' : 'El nombre solo debe contener letras y espacios.';
+    $errores['acreditado_folio'] = $bitacora['acreditado_folio'] ? '' : 'El número de expediente debe comenzar con «IYE» y contener números y guiones.';
+    $errores['acreditado_municipio'] = $bitacora['acreditado_municipio'] ? '' : 'Este campo es requerido';
+    $errores['acreditado_garantia'] = $bitacora['acreditado_garantia'] ? '' : 'Este campo es requerido.';
+    $errores['acreditado_telefono'] = $bitacora['acreditado_telefono'] ? '' : 'El número de teléfono debe tener un formato correcto';
+    $errores['acreditado_email'] = $bitacora['acreditado_email'] ? '' : 'Introduzca un correo electrónico con formato válido.';
+    $errores['acreditado_direccion_negocio'] = $bitacora['acreditado_direccion_negocio'] ? '' : 'Este campo es requerido';
+    $errores['acreditado_direccion_particular'] = $bitacora['acreditado_direccion_particular'] ? '' : 'Este campo es requerido';
+    $errores['gestion_fecha1'] = $bitacora['gestion_fecha1'] ? '' : 'Introduzca un formato de fecha válido.';
+    $errores['gestion_via1'] = $bitacora['gestion_via1'] ? '' : 'Seleccione una opción válida.';
+
+    $generacion_invalida = implode($errores);
+
+    if (!$generacion_invalida) {
+
+// Create variable with filename
+        $nombre_archivo = $bitacora['acreditado_folio'] . ' ' . $bitacora['acreditado_nombre'] . ' - Bitácora.docx';
+// Encode filename so that UTF-8 characters work
+        $nombre_archivo_decodificado = rawurlencode($nombre_archivo);
+
+// Create new instance of PHPWord template processor using the required template file
+        $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor('./plantillas/plantilla-bitacora.docx');
+
+// Set values in template with post received input variables
+        $templateProcessor->setValue('acreditado_nombre', $bitacora['acreditado_nombre']);
+        $templateProcessor->setValue('acreditado_folio', $bitacora['acreditado_folio']);
+        $templateProcessor->setValue('acreditado_municipio', $bitacora['acreditado_municipio']);
+        $templateProcessor->setValue('acreditado_garantia', $bitacora['acreditado_garantia']);
+        $templateProcessor->setValue('acreditado_telefono', $bitacora['acreditado_telefono']);
+        $templateProcessor->setValue('acreditado_email', $bitacora['acreditado_email']);
+        $templateProcessor->setValue('acreditado_direccion_negocio', $bitacora['acreditado_direccion_negocio']);
+        $templateProcessor->setValue('acreditado_direccion_particular', $bitacora['acreditado_direccion_particular']);
+        $templateProcessor->setValue('aval_nombre', $bitacora['aval_nombre']);
+        $templateProcessor->setValue('aval_telefono', $bitacora['aval_telefono']);
+        $templateProcessor->setValue('aval_email', $bitacora['aval_email']);
+        $templateProcessor->setValue('aval_direccion', $bitacora['aval_direccion']);
+        $templateProcessor->setValue('gestion_fecha1', date("d-m-Y", strtotime($bitacora['gestion_fecha1'])));
+        $templateProcessor->setValue('gestion_via1', $bitacora['gestion_via1']);
+        $templateProcessor->setValue('gestion_comentarios1', $bitacora['gestion_comentarios1']);
+        for ($i = 2; $i <= 12; $i++) {
+            $templateProcessor->setValue('gestion_fecha' . $i, '');
+            $templateProcessor->setValue('gestion_via' . $i, '');
+            $templateProcessor->setValue('gestion_comentarios' . $i, '');
+        }
+        $templateProcessor->setValue('evidencia_fecha', $bitacora['evidencia_fecha1_texto']);
+        if ($movido) {
+            $templateProcessor->setImageValue('evidencia_fotografia', array('path' => './uploads/' . $bitacora['evidencia_fotografia1'], 'width' => 720, 'height' => 480));
+        } else {
+            $templateProcessor->setValue('evidencia_fotografia', '');
+        }
+// Escape strings to insert into the database table
+        $acreditado_nombre = mysqli_real_escape_string($conn, $_POST['acreditado_nombre']);
+        $folio = mysqli_real_escape_string($conn, $_POST['folio']);
+        $municipio = mysqli_real_escape_string($conn, $_POST['municipio']);
+        $garantia = mysqli_real_escape_string($conn, $_POST['garantia']);
+        $acreditado_telefono = mysqli_real_escape_string($conn, $_POST['acreditado_telefono']);
+        $acreditado_email = mysqli_real_escape_string($conn, $_POST['acreditado_email']);
+        $direccion_negocio = mysqli_real_escape_string($conn, $_POST['direccion_negocio']);
+        $direccion_particular = mysqli_real_escape_string($conn, $_POST['direccion_particular']);
+        $aval_nombre = mysqli_real_escape_string($conn, $_POST['aval_nombre']);
+        $aval_telefono = mysqli_real_escape_string($conn, $_POST['aval_telefono']);
+        $aval_email = mysqli_real_escape_string($conn, $_POST['aval_email']);
+        $aval_direccion = mysqli_real_escape_string($conn, $_POST['aval_direccion']);
+        $gestion_fecha = mysqli_real_escape_string($conn, $_POST['gestion_fecha1']);
+        $gestion_via = mysqli_real_escape_string($conn, $_POST['gestion_via1']);
+        $gestion_comentarios = mysqli_real_escape_string($conn, $_POST['gestion_comentarios1']);
+        $evidencia_fecha = mysqli_real_escape_string($conn, $_POST['evidencia_fecha1']);
+        $evidencia_fotografia = mysqli_real_escape_string($conn, $_FILES['evidencia_fotografia1']['name']);
+
+// Query
+        $sql = "INSERT INTO bitacora(acreditado_nombre, acreditado_folio, acreditado_municipio, acreditado_garantia, acreditado_telefono, acreditado_email,
+                     acreditado_direccion_negocio, acreditado_direccion_particular, aval_nombre, aval_telefono, aval_email, aval_direccion,
+                     gestion_fecha1, gestion_via1, gestion_comentarios1, evidencia_fecha1, evidencia_fotografia1,
+                     nombre_archivo, gestion_contador) VALUES('$acreditado_nombre', '$folio', '$municipio', '$garantia', '$acreditado_telefono', '$acreditado_email',
+                                            '$direccion_negocio', '$direccion_particular', '$aval_nombre', '$aval_telefono', '$aval_email', '$aval_direccion', '$gestion_fecha',
+                                            '$gestion_via', '$gestion_comentarios', '$evidencia_fecha', '$evidencia_fotografia', '$nombre_archivo', 1);";
+
+// Validation of query
+        if (mysqli_query($conn, $sql)) {
+
+            // Path where generated file is saved
+            $ruta_guardado = './files/bitacoras/' . $nombre_archivo;
+            $templateProcessor->saveAs($ruta_guardado);
+
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+            header('Content-Disposition: attachment; filename="' . "$nombre_archivo_decodificado" . '"');
+            header('Content-Transfer-Encoding: binary');
+
+            ob_clean();
+            flush();
+            // Send generated file stored in the server to the browser
+            readfile($ruta_guardado);
+            exit;
+        } else {
+            echo 'Error de consulta: ' . mysqli_error($conn);
+        }
+    }
 }
 ?>
 <!doctype html>
@@ -69,7 +281,7 @@ if (!isset($_SESSION['login'])) {
                 </a>
             </div>
             <div>
-                <form class="form" action="exportar-bitacora.php" method="post" enctype="multipart/form-data">
+                <form class="form" action="generador-bitacora.php" method="post" enctype="multipart/form-data">
                     <fieldset class="form__fieldset form__fieldset--accreditedLogbook">
                         <legend class="form__legend">Datos del acreditado</legend>
                         <div class="form__division">
@@ -79,21 +291,22 @@ if (!isset($_SESSION['login'])) {
                                    name="acreditado_nombre" required>
                         </div>
                         <div class="form__division">
-                            <label class="form__label" for="folio">Folio<span
+                            <label class="form__label" for="acreditado_folio">Folio<span
                                         class="asterisk">*</span>: </label>
-                            <input class="form__input" type="text" id="folio"
-                                   name="folio"
+                            <input class="form__input" type="text" id="acreditado_folio"
+                                   name="acreditado_folio"
                                    required>
                         </div>
                         <div class="form__division">
-                            <label class="form__label" for="municipio">Municipio<span
+                            <label class="form__label" for="acreditado_municipio">Municipio<span
                                         class="asterisk">*</span>:</label>
-                            <input class="form__input" type="text" id="municipio" name="municipio">
+                            <input class="form__input" type="text" id="acreditado_municipio"
+                                   name="acreditado_municipio">
                         </div>
                         <div class="form__division">
-                            <label class="form__label" for="garantia">Garantía<span
+                            <label class="form__label" for="acreditado_garantia">Garantía<span
                                         class="asterisk">*</span>:</label>
-                            <input class="form__input" type="text" id="garantia" name="garantia">
+                            <input class="form__input" type="text" id="acreditado_garantia" name="acreditado_garantia">
                         </div>
                         <div class="form__division">
                             <label class="form__label" for="acreditado_telefono">Teléfono<span
@@ -108,17 +321,19 @@ if (!isset($_SESSION['login'])) {
                                    name="acreditado_email">
                         </div>
                         <div class="form__division">
-                            <label class="form__label" for="direccion_negocio">Dirección del negocio<span
+                            <label class="form__label" for="acreditado_direccion_negocio">Dirección del negocio<span
                                         class="asterisk">*</span>:
                             </label>
-                            <input class="form__input" type="text" id="direccion_negocio" name="direccion_negocio"
+                            <input class="form__input" type="text" id="acreditado_direccion_negocio"
+                                   name="acreditado_direccion_negocio"
                                    required>
                         </div>
                         <div class="form__division">
-                            <label class="form__label" for="direccion_particular">Dirección particular<span
+                            <label class="form__label" for="acreditado_direccion_particular">Dirección particular<span
                                         class="asterisk">*</span>:
                             </label>
-                            <input class="form__input" type="text" id="direccion_particular" name="direccion_particular"
+                            <input class="form__input" type="text" id="acreditado_direccion_particular"
+                                   name="acreditado_direccion_particular"
                                    required>
                         </div>
                     </fieldset>
@@ -162,13 +377,17 @@ if (!isset($_SESSION['login'])) {
                             <label class="form__label" for="gestion_via1">Vía<span
                                         class="asterisk">*</span>:
                             </label>
-                            <input class="form__input" type="text" id="gestion_via1" name="gestion_via1"
-                                   required>
+                            <select class="form__input" id="gestion_via1" name="gestion_via1" required>
+                                <?php foreach ($tipos_gestion as $vias) : ?>
+                                    <option value="<?= htmlspecialchars($vias) ?>" <?= $bitacora['gestion_via1'] === $vias ? 'selected' : '' ?>><?= htmlspecialchars($vias) ?></option>
+                                <?php endforeach; ?>
+                            </select>
                         </div>
                         <div class="form__division">
                             <label class="form__label" for="gestion_comentarios1">Comentarios/Resultados:
                             </label>
-                            <input class="form__input" type="text" id="gestion_comentarios1" name="gestion_comentarios1">
+                            <input class="form__input" type="text" id="gestion_comentarios1"
+                                   name="gestion_comentarios1">
                         </div>
                     </fieldset>
                     <fieldset class="form__fieldset form__fieldset--evidence">
