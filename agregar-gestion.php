6985cbc17bdad4368e3ac7b5239e5aa063b8a168
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-09-06 00:44:46 -0500

    Fix bug with images in Word template

--- agregar-gestion.php
+++ agregar-gestion.php
@@ -152,7 +152,11 @@ if ($_GET['id']) {
                         for ($i = 1; $i < $new_counter; $i++) {
                             $templateProcessor->setValue('evidencia_fecha#' . $i, $bitacora[0]['evidencia_fecha' . $i] ? "Se visitó el negocio el " . datefmt_format($fmt, new DateTime($bitacora[0]['evidencia_fecha' . $i], $tz_CMX)) . ".</w:t><w:br/><w:t>Fachada del negocio." : '');
                             if ($bitacora[0]['evidencia_fotografia' . $i]) {
-                                $templateProcessor->setImageValue('evidencia_fotografia#' . $i, array('path' => $ruta_subido . $bitacora[0]['evidencia_fotografia' . $i], 'width' => 720, 'height' => 480));
+                                if (file_exists($ruta_subido . $bitacora[0]['evidencia_fotografia' . $i])) {
+                                    $templateProcessor->setImageValue('evidencia_fotografia#' . $i, array('path' => $ruta_subido . $bitacora[0]['evidencia_fotografia' . $i], 'width' => 720, 'height' => 480));
+                                } else {
+                                    $templateProcessor->setValue('evidencia_fotografia#' . $i, '');
+                                }
                             } else {
                                 $templateProcessor->setValue('evidencia_fotografia#' . $i, '');
                             }
@@ -164,7 +168,11 @@ if ($_GET['id']) {
                         for ($i = 1; $i <= $new_counter - 1; $i++) {
                             $templateProcessor->setValue('evidencia_fecha#' . $i, $bitacora[0]['evidencia_fecha' . $i] ? "Se visitó el negocio el " . datefmt_format($fmt, new DateTime($bitacora[0]['evidencia_fecha' . $i], $tz_CMX)) . ".</w:t><w:br/><w:t>Fachada del negocio." : '');
                             if ($bitacora[0]['evidencia_fotografia' . $i]) {
-                                $templateProcessor->setImageValue('evidencia_fotografia#' . $i, array('path' => $ruta_subido . $bitacora[0]['evidencia_fotografia' . $i], 'width' => 720, 'height' => 480));
+                                if (file_exists($ruta_subido . $bitacora[0]['evidencia_fotografia' . $i])) {
+                                    $templateProcessor->setImageValue('evidencia_fotografia#' . $i, array('path' => $ruta_subido . $bitacora[0]['evidencia_fotografia' . $i], 'width' => 720, 'height' => 480));
+                                } else {
+                                    $templateProcessor->setValue('evidencia_fotografia#' . $i, '');
+                                }
                             } else {
                                 $templateProcessor->setValue('evidencia_fotografia#' . $i, '');
                             }
