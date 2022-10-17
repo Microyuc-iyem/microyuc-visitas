a36bca9e1438762154c1454e1d5181394b12670e
Diego Martinez <115640081+DiegoMtR8@users.noreply.github.com>
2022-10-17 14:54:41 -0500

    update modo cleared

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,15 +1,13 @@
 <?php
 //Get Heroku ClearDB connection information
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$cleardb_server = "us-cdbr-east-06.cleardb.net";
-$cleardb_username = "b8b05bd297e7a4";
-$cleardb_password = "50b32aa6";
-$cleardb_name = "heroku_575502a958f4fa8" ;
+$server = $url["us-cdbr-east-06.cleardb.net"];
+$username = $url["b8b05bd297e7a4"];
+$password = $url["50b32aa6"];
+$db = substr($url["heroku_575502a958f4fa8"], 1);
+
 
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_name);
-
-
-if (!$conn) {
-    echo 'Connection error: ' . mysqli_connect_error();
-}
+$conn = new mysqli($server, $username, $password, $db);
+?>
