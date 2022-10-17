5a99fbc3fb222bd526295500d3308ee944042dab
Diego Martinez <115640081+DiegoMtR8@users.noreply.github.com>
2022-10-17 18:58:25 -0500

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,10 +1,10 @@
 <?php
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["us-cdbr-east-06.cleardb.net"];
-$cleardb_username = $cleardb_url["b8b05bd297e7a4"];
-$cleardb_password = $cleardb_url["50b32aa6"];
-$cleardb_db = substr($cleardb_url["heroku_575502a958f4fa8"],1);
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 
