38f051ff9feb870bc65202bfa6eab379e598115f
Diego Martinez <115640081+DiegoMtR8@users.noreply.github.com>
2022-10-17 14:45:46 -0500

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -2,15 +2,16 @@
 //Get Heroku ClearDB connection information
 
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
+$cleardb_server = "us-cdbr-east-06.cleardb.net";
+$cleardb_username = "b8b05bd297e7a4";
+$cleardb_password = "50b32aa6";
+$cleardb_name = "heroku_575502a958f4fa8" ;
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_name);
 //$conn = mysqli_connect('localhost', 'microyuc', '1234', 'microyuc_project');
 
 if (!$conn) {
