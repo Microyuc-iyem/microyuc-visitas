81620a9c829911ccf52e983e48e278391d1dd303
Diego Martinez <115640081+DiegoMtR8@users.noreply.github.com>
2022-10-17 19:02:47 -0500

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -7,11 +7,6 @@ $cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
-
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-//$conn = mysqli_connect('localhost', 'sig', '1234', 'microyuc_project');
-
-if (!$conn) {
-    echo 'Connection error: ' . mysqli_connect_error();
-}
+?>
