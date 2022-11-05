1832ed9e52fe11581a0c69760e12f7c84592bf9c
Diego Martinez <115640081+DiegoMtR8@users.noreply.github.com>
2022-11-05 16:40:58 -0600

    Update generador-carta.php

--- generador-carta.php
+++ generador-carta.php
@@ -99,7 +99,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $filtros['documentacion']['options']['regexp'] = '/[\s\S]+/';
     $filtros['documentacion']['options']['default'] = '';
     $filtros['comprobacion_monto']['filter'] = FILTER_VALIDATE_FLOAT;
-    $filtros['comprobacion_monto']['options']['max_range'] = 1000000.00;
+    $filtros['comprobacion_monto']['options']['default'] = '';
     $filtros['capital_de_trabajo']['filter'] = FILTER_VALIDATE_REGEXP;
     $filtros['capital_de_trabajo']['options']['regexp'] = '/[\s\S]+/';
     $filtros['activo_fijo']['filter'] = FILTER_VALIDATE_REGEXP;
@@ -147,7 +147,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $errores['municipio'] = $carta['municipio'] ? '' : 'Este campo es requerido.';
     $errores['fecha_firma'] = $carta['fecha_firma'] ? '' : 'Por favor, introduzca un formato de fecha válido.';
     $errores['comprobacion_monto'] = $carta['comprobacion_monto'] ? '' : 'El monto debe ser mayor o igual a 0';
-    if (is_null($carta['capital_de_trabajo']) && is_null($carta['activo_fijo']) && is_null($carta['adecuaciones']) && is_null($carta['insumos']) && is_null($carta['certificaciones']) && is_null($carta['sin_comprobacion']) ) {
+    if (is_null($carta['capital_de_trabajo']) && is_null($carta['activo_fijo']) && is_null($carta['adecuaciones']) && is_null($carta['sin_comprobacion']) && is_null($carta['insumos']) && is_null($carta['certificaciones'])) {
         $errores['comprobacion_tipo'] = 'Por favor, seleccione al menos una opción.';
     } else {
         $errores['comprobacion_tipo'] = '';
@@ -396,7 +396,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                                 class="asterisk">*</span>:
                     </label>
                     <input class="form__input" type="number" id="comprobacion_monto"
-                           name="comprobacion_monto" step="0.00" min="0"
+                           name="comprobacion_monto" step="0.01" min="-1"
                            value="<?= htmlspecialchars($carta['comprobacion_monto']) ?>" required>
                 </div>
                 <div class="form__division">
