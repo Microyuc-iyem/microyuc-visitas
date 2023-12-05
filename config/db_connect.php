560cfb9859235a345c31daebcfce8067f19537f9
Diego Martinez <115640081+Microyuc-iyem@users.noreply.github.com>
2023-12-05 14:12:07 -0600

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -10,11 +10,6 @@
 
 $conn = pg_connect("host=ec2-52-1-92-133.compute-1.amazonaws.com dbname=d8klmol62f7oi1 user=djgymmheoobtax password=7332bb6610847d90872b35e6ff411d5379628c314f848699b4aff90ac9029e76");
 
-$result = pg_query($conn, "SELECT * FROM usuarios");
-
-while ($row = pg_fetch_row($result)) {
-  var_dump($row);
-}
 
 pg_close($conn);
 
