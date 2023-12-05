c3dad0c840e28aec5f6c1fc6b5c776f5a628fcdf
Diego Martinez <115640081+Microyuc-iyem@users.noreply.github.com>
2023-12-05 14:14:32 -0600

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -14,6 +14,10 @@ $password = "tu_contraseña";
 
 $conn = pg_connect("host=ec2-52-1-92-133.compute-1.amazonaws.com dbname=d8klmol62f7oi1 user=djgymmheoobtax password=7332bb6610847d90872b35e6ff411d5379628c314f848699b4aff90ac9029e76");
 
+// Verifica la conexión
+if (!$conn) {
+    die("Error de conexión: " . pg_last_error());
+}
 
 pg_close($conn);
 ?>
