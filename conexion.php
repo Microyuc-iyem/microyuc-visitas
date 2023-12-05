3f77a5df763fd9971b9da343252a4ba4c738ffcd
Diego Martinez <115640081+Microyuc-iyem@users.noreply.github.com>
2023-12-05 17:48:29 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -12,7 +12,10 @@ $password = "7332bb6610847d90872b35e6ff411d5379628c314f848699b4aff90ac9029e76";
 $conn = pg_connect("host=ec2-52-1-92-133.compute-1.amazonaws.com dbname=d8klmol62f7oi1 user=djgymmheoobtax password=7332bb6610847d90872b35e6ff411d5379628c314f848699b4aff90ac9029e76");
 
 // Verificar la conexión
-if (!$conn) {
+if (!$conn) { 
+    echo("JEsucristo); }
+    else {
+    
     die("Error de conexión: " . pg_last_error());
 }
 
