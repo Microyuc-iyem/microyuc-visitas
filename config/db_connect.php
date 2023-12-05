b9c4c552a9ac6249fda87e2bba4537567fafa95e
Diego Martinez <115640081+Microyuc-iyem@users.noreply.github.com>
2023-12-05 14:06:50 -0600

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,14 +1,5 @@
 
-//Get Heroku ClearDB connection information
-//$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-//$cleardb_server = $cleardb_url["ec2-52-1-92-133.compute-1.amazonaws.com"];
-//$cleardb_username = $cleardb_url["user"];
-//$cleardb_password = $cleardb_url["pass"];
-//$cleardb_db = substr($cleardb_url["path"],1);
-//$active_group = 'default';
-//query_builder = TRUE;
-// Connect to DB
-//$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 
 <?php
 //$host = "ec2-52-1-92-133.compute-1.amazonaws.com";
