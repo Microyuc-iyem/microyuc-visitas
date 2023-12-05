0c13b52ba2ba4fb9d168d481d60dca2c6285fbf6
Diego Martinez <115640081+Microyuc-iyem@users.noreply.github.com>
2023-12-05 14:27:24 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,1 +1,18 @@
 
+<?php
+// Configuración de la conexión a la base de datos
+$host = "ec2-52-1-92-133.compute-1.amazonaws.com";
+$port = "5432";
+$dbname = "d8klmol62f7oi1";
+$user = "djgymmheoobtax";
+$password = "7332bb6610847d90872b35e6ff411d5379628c314f848699b4aff90ac9029e76";
+
+// Establecer la conexión
+
+$conn = pg_connect("host=ec2-52-1-92-133.compute-1.amazonaws.com dbname=d8klmol62f7oi1 user=djgymmheoobtax password=7332bb6610847d90872b35e6ff411d5379628c314f848699b4aff90ac9029e76");
+
+// Verifica la conexión
+if (!$conn) {
+    die("Error de conexión: " . pg_last_error());
+}
+?>
