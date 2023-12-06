9cc7e1dae950c4503def2c354f99ed59a54c736e
Diego Martinez <115640081+Microyuc-iyem@users.noreply.github.com>
2023-12-06 11:40:19 -0600

    Update db_connect.php
    BASE DE DATOS MYSQL

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,11 +1,11 @@
 
 <?php
 // Configuración de la conexión a la base de datos
-$host = "ec2-52-1-92-133.compute-1.amazonaws.com";
-$port = "5432";
-$dbname = "d8klmol62f7oi1";
-$user = "djgymmheoobtax";
-$password = "7332bb6610847d90872b35e6ff411d5379628c314f848699b4aff90ac9029e76";
+$host = "d6rii63wp64rsfb5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$port = "3306";
+$dbname = "xa5i7us8zbh7oqz7";
+$user = "qybk61orec077c8s";
+$password = "jwd9jkw5u0fgmd7i";
 
 // Establecer la conexión
 
