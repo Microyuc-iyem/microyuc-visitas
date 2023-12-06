7e48a37a71f3a27fc0a84bc06aa71b27a2b1593f
Diego Martinez <115640081+Microyuc-iyem@users.noreply.github.com>
2023-12-06 13:27:13 -0600

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -48,7 +48,7 @@
                                       d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
                             </svg>
                             <span>Bitácoras</span></a></li>
-                    <li><a href="./generador-carta.php"
+                    <li><a href="./generador-carta-aval.php"
                            class="sidebar__link <?php if (isset($sidebar_active)) {echo $sidebar_active == 'carta' ? 'sidebar__link--active' : '';} else { echo ''; } ?>">
                             <svg xmlns="http://www.w3.org/2000/svg" class="sidebar__icon" fill="none"
                                  viewBox="0 0 24 24"
