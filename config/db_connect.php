1eb3f6035c2048f41a8cbd6efcffd2fc01e34b30
Diego Martinez <115640081+Microyuc-iyem@users.noreply.github.com>
2023-12-06 11:42:20 -0600

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -9,10 +9,10 @@ $password = "jwd9jkw5u0fgmd7i";
 
 // Establecer la conexión
 
-$conn = pg_connect("host=ec2-52-1-92-133.compute-1.amazonaws.com dbname=d8klmol62f7oi1 user=djgymmheoobtax password=7332bb6610847d90872b35e6ff411d5379628c314f848699b4aff90ac9029e76");
+// Connect to DB
+$conn = mysqli_connect($d6rii63wp64rsfb5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com, $qybk61orec077c8s, $jwd9jkw5u0fgmd7i, $xa5i7us8zbh7oqz7);
+//$conn = mysqli_connect('localhost', 'sig', '1234', 'microyuc_project');
 
-// Verifica la conexión
 if (!$conn) {
-    die("Error de conexión: " . pg_last_error());
+    echo 'Connection error: ' . mysqli_connect_error();
 }
-?>
