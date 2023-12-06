a3a057bb7fd93466d411ef99d85fde2965624e81
Diego Martinez <115640081+Microyuc-iyem@users.noreply.github.com>
2023-12-06 12:28:09 -0600

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'conexion.php';
+require_once './config/db_connect.php';
 require_once './includes/functions.php';
 
 $sidebar_active = 'inicio';
@@ -24,9 +24,9 @@ require_once './includes/header.php';
         <a href="cartas.php" class="card__number">
             <?php
             $dash_carta_query = "SELECT * FROM carta";
-            $dash_carta_query_run = pg_query($conn, $dash_carta_query);
+            $dash_carta_query_run = mysqli_query($conn, $dash_carta_query);
 
-            if ($cartas_total = pg_num_rows($dash_carta_query_run)) {
+            if ($cartas_total = mysqli_num_rows($dash_carta_query_run)) {
                 echo $cartas_total;
             } else {
                 echo "Sin datos";
@@ -35,6 +35,32 @@ require_once './includes/header.php';
         </a>
     </div>
     <div class="dashboard__card">
+        <h2 class="card__title">
+            <svg xmlns="http://www.w3.org/2000/svg" class="card__icon" fill="none"
+                 viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
+            </svg>
+            Bitácoras generadas
+        </h2>
+        <a href="bitacoras.php" class="card__number">
+            <?php
+            $dash_bitacora_query = "SELECT * FROM bitacora";
+            $dash_bitacora_query_run = mysqli_query($conn, $dash_bitacora_query);
+
+            if ($bitacoras_total = mysqli_num_rows($dash_bitacora_query_run)) {
+                echo $bitacoras_total;
+            } else {
+                echo "Sin datos";
+            }
+            ?>
+        </a>
+    </div>
+</div>
+</main>
+</div>
+</body>
+</html>
         <h2 class="card__title">
             <svg xmlns="http://www.w3.org/2000/svg" class="card__icon" fill="none"
                  viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
