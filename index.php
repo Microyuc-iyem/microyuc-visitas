f813f7151066d25d56e9cdc6c53d58176d715260
Diego Martinez <115640081+Microyuc-iyem@users.noreply.github.com>
2023-12-06 11:28:56 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-<?php
+
+
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="dist/css/styles.css">
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <title>Microyuc Emprendedores | Inicio de Sesión</title>
+</head>
+<body>
+    <?php
 session_start();
 require_once 'conexion.php';
 
@@ -22,19 +36,6 @@ if (isset($_SESSION['login'])) {
         }
     }
     ?>
-
-<!doctype html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="dist/css/styles.css">
-    <link rel="icon" type="image/x-icon" href="./favicon.ico">
-    <title>Microyuc Emprendedores | Inicio de Sesión</title>
-</head>
-<body>
 <div class="login">
     <img src="img/microyucfondo.png" alt="Logo de Microyuc" class="login__img">
     <div class="login__container">
