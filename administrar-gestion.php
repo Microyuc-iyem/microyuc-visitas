b424696be758e9ede8b8117313a67aef7e704a9c
Diego Martinez <115640081+Microyuc-iyem@users.noreply.github.com>
2024-02-12 15:19:11 -0600

    Update administrar-gestion.php
    Prueba para ordenar fechas bitacoras

--- administrar-gestion.php
+++ administrar-gestion.php
@@ -57,7 +57,8 @@ if ($_GET['id']) {
 // Create new instance of PHPWord template processor using the required template file
                 $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor('./plantillas/plantilla-bitacora.docx');
 
-                $sql = "SELECT * FROM bitacora WHERE id = " . $_GET['id'] . ";";
+                $sql = "SELECT * FROM bitacora WHERE id = " . $_GET['id'] . " ORDER BY gestion_fecha DESC;";
+
                 $result = mysqli_query($conn, $sql);
                 // Fetch the resulting rows as an associative array
                 $bitacoras = mysqli_fetch_all($result, MYSQLI_ASSOC);
