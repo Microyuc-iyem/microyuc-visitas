1468f8f4dc62352e73135d88b683812f1f9485b0
Diego Martinez <115640081+Microyuc-iyem@users.noreply.github.com>
2024-03-07 12:40:28 -0600

    Update agregar-gestion.php

--- agregar-gestion.php
+++ agregar-gestion.php
@@ -119,12 +119,12 @@ if ($_GET['id']) {
                     $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor('./plantillas/plantilla-bitacora.docx');
 
 // Set values in template with post received input variables
-                    $values = [];
-                    for ($i = 1; $i < $new_counter; $i++) {
-                        if ($bitacora[0]['gestion_fecha' . $i]) {
-                            $values[] = ['gestion_fecha' => date("d-m-Y", strtotime($bitacora[0]['gestion_fecha' . $i])), 'gestion_via' => $bitacora[0]['gestion_via' . $i], 'gestion_comentarios' => $bitacora[0]['gestion_comentarios' . $i]];
-                        }
-                    }
+                 //   $values = [];
+                   // for ($i = 1; $i < $new_counter; $i++) {
+                     //   if ($bitacora[0]['gestion_fecha' . $i]) {
+                      //      $values[] = ['gestion_fecha' => date("d-m-Y", strtotime($bitacora[0]['gestion_fecha' . $i])), 'gestion_via' => $bitacora[0]['gestion_via' . $i], 'gestion_comentarios' => $bitacora[0]['gestion_comentarios' . $i]];
+                       // }
+                    //}
 
                     $values[] = ['gestion_fecha' => date("d-m-Y", strtotime($gestion['gestion_fecha' . $new_counter])), 'gestion_via' => $gestion['gestion_via' . $new_counter], 'gestion_comentarios' => $gestion['gestion_comentarios' . $new_counter]];
 
@@ -138,8 +138,46 @@ if ($_GET['id']) {
 
 
 
+// Define un arreglo para almacenar todas las gestiones
+$values = [];
+
+// Itera sobre las gestiones
+for ($i = 1; $i <= $new_counter; $i++) {
+    // Verifica si existe la fecha de la gestión
+    if ($bitacora[0]['gestion_fecha' . $i]) {
+        // Agrega la gestión al arreglo de valores
+        $values[] = [
+            'fecha' => strtotime($bitacora[0]['gestion_fecha' . $i]), // Convierte la fecha a un timestamp para facilitar la comparación
+            'via' => $bitacora[0]['gestion_via' . $i],
+            'comentarios' => $bitacora[0]['gestion_comentarios' . $i]
+        ];
+    }
+}
+
+// Agrega la última gestión (la que se está procesando actualmente)
+$values[] = [
+    'fecha' => strtotime($gestion['gestion_fecha' . $new_counter]),
+    'via' => $gestion['gestion_via' . $new_counter],
+    'comentarios' => $gestion['gestion_comentarios' . $new_counter]
+];
+
+// Define una función de comparación para ordenar las gestiones por fecha
+function compararFechas($a, $b) {
+    return $a['fecha'] - $b['fecha'];
+}
+
+// Ordena las gestiones por fecha
+usort($values, 'compararFechas');
 
 
+    $values[] = ['gestion_fecha' => date("d-m-Y", strtotime($gestion['gestion_fecha' . $new_counter])), 'gestion_via' => $gestion['gestion_via' . $new_counter], 'gestion_comentarios' => $gestion['gestion_comentarios' . $new_counter]];
+
+                    $AT_gestion_fecha = 'gestion_fecha' . $new_counter;
+                    $AT_gestion_via = 'gestion_via' . $new_counter;
+                    $AT_gestion_comentarios = 'gestion_comentarios' . $new_counter;
+
+                    $AT_query = "ALTER TABLE bitacora ADD " . $AT_gestion_fecha . " VARCHAR(255) DEFAULT '', ADD " . $AT_gestion_via . " VARCHAR(255) DEFAULT '', ADD " . $AT_gestion_comentarios . " VARCHAR(255) DEFAULT ''";
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
