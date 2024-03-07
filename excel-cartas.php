8cfc26866e5c39a57eb56c6e784e50a11fab190c
Diego Martinez <115640081+Microyuc-iyem@users.noreply.github.com>
2024-03-07 10:53:19 -0600

    Update excel-cartas.php

--- excel-cartas.php
+++ excel-cartas.php
@@ -11,7 +11,7 @@ $cartas = [
     ['<b>N.°</b>', '<b>Fecha de creación</b>', '<b>Fecha de visita</b>', '<b>Folio</b>', '<b>Nombre</b>', '<b>Colonia/Fraccionamiento', '<b>Localidad</b>', '<b>Municipio</b>', '<b>Fecha de firma de anexos</b>', '<b>Documentación</b>', '<b>Monto de comprobación</b>', '<b>Tipo de comprobación</b>', '<b>Fecha de pago inicial</b>', '<b>Fecha de pago final</b>', '<b>Modalidad</b>', '<b>Tipo de crédito</b>', '<b>Fecha de otorgamiento</b>', '<b>Monto inicial</b>', '<b>Mensualidades vencidas</b>', '<b>Adeudo total</b>',],
 ];
 
-$sql = 'SELECT * FROM cartas;';
+$sql = 'SELECT * FROM carta;';
 $res = mysqli_query($conn, $sql);
 if (mysqli_num_rows($res) > 0) {
     foreach ($res as $row) {
@@ -29,15 +29,15 @@ for ($i = 1; $i < count($cartas); $i++) {
     $cartas[$i][15] = date('m-Y', strtotime($cartas[$i][15]));
     $cartas[$i][16] = date('m-Y', strtotime($cartas[$i][16]));
     $cartas[$i][19] = date('d-m-Y', strtotime($cartas[$i][19]));
-    $cartas[$i][20] = number_format(intval($cartas[$i][20]), 2);
-    $cartas[$i][20] = number_format(intval($cartas[$i][20]), 2);
+    $cartas[$i][20] = number_format($cartas[$i][20], 2);
+    $cartas[$i][22] = number_format($cartas[$i][22], 2);
     unset($cartas[$i][5]);
     unset($cartas[$i][6]);
     unset($cartas[$i][7]);
     unset($cartas[$i][23]);
 }
 
-$filename = 'Reporte de cartas.xlsx';
+$filename = 'Reporte de cartas ' . $current_timestamp . '.xlsx';
 
 $xlsx = Shuchkin\SimpleXLSXGen::fromArray($cartas);
 $xlsx->downloadAs($filename);
