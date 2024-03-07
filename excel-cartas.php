dbc3c1617e833d7ece033fca5336940a7452add2
Diego Martinez <115640081+Microyuc-iyem@users.noreply.github.com>
2024-03-07 10:42:53 -0600

    Update excel-cartas.php

--- excel-cartas.php
+++ excel-cartas.php
@@ -6,12 +6,12 @@ $tz_CMX = new DateTimeZone('America/Mexico_City');
 $CMX = new DateTime('now', $tz_CMX);
 $current_timestamp = $CMX->format('d-m-Y');
 
-$cartas = [];
-$cartas = [
+$aval = [];
+$aval = [
     ['<b>N.°</b>', '<b>Fecha de creación</b>', '<b>Fecha de visita</b>', '<b>Folio</b>', '<b>Nombre</b>', '<b>Colonia/Fraccionamiento', '<b>Localidad</b>', '<b>Municipio</b>', '<b>Fecha de firma de anexos</b>', '<b>Documentación</b>', '<b>Monto de comprobación</b>', '<b>Tipo de comprobación</b>', '<b>Fecha de pago inicial</b>', '<b>Fecha de pago final</b>', '<b>Modalidad</b>', '<b>Tipo de crédito</b>', '<b>Fecha de otorgamiento</b>', '<b>Monto inicial</b>', '<b>Mensualidades vencidas</b>', '<b>Adeudo total</b>',],
 ];
 
-$sql = 'SELECT * FROM carta;';
+$sql = 'SELECT * FROM aval;';
 $res = mysqli_query($conn, $sql);
 if (mysqli_num_rows($res) > 0) {
     foreach ($res as $row) {
@@ -19,25 +19,25 @@ if (mysqli_num_rows($res) > 0) {
     }
 }
 
-for ($i = 1; $i < count($cartas); $i++) {
-    $cartas[$i][0] = '<b>' . $i . '</b>';
-    $cartas[$i][1] = date('d-m-Y H:i:s', strtotime($cartas[$i][1]));
-    $cartas[$i][2] = $cartas[$i][2] ? date('d-m-Y', strtotime($cartas[$i][2])) : '';
-    $cartas[$i][11] = date('d-m-Y', strtotime($cartas[$i][11]));
-    $cartas[$i][13] = number_format($cartas[$i][13], 2);
-    $cartas[$i][14] = ucfirst($cartas[$i][14]);
-    $cartas[$i][15] = date('m-Y', strtotime($cartas[$i][15]));
-    $cartas[$i][16] = date('m-Y', strtotime($cartas[$i][16]));
-    $cartas[$i][19] = date('d-m-Y', strtotime($cartas[$i][19]));
-    $cartas[$i][20] = number_format(intval($cartas[$i][20]), 2);
-    $cartas[$i][20] = number_format(intval($cartas[$i][20]), 2);
-    unset($cartas[$i][5]);
-    unset($cartas[$i][6]);
-    unset($cartas[$i][7]);
-    unset($cartas[$i][23]);
+for ($i = 1; $i < count($aval); $i++) {
+    $aval[$i][0] = '<b>' . $i . '</b>';
+    $aval[$i][1] = date('d-m-Y H:i:s', strtotime($aval[$i][1]));
+    $aval[$i][2] = $aval[$i][2] ? date('d-m-Y', strtotime($aval[$i][2])) : '';
+    $aval[$i][11] = date('d-m-Y', strtotime($aval[$i][11]));
+    $aval[$i][13] = number_format($aval[$i][13], 2);
+    $aval[$i][14] = ucfirst($aval[$i][14]);
+    $aval[$i][15] = date('m-Y', strtotime($aval[$i][15]));
+    $aval[$i][16] = date('m-Y', strtotime($aval[$i][16]));
+    $aval[$i][19] = date('d-m-Y', strtotime($aval[$i][19]));
+    $aval[$i][20] = number_format(intval($aval[$i][20]), 2);
+    $aval[$i][20] = number_format(intval($aval[$i][20]), 2);
+    unset($aval[$i][5]);
+    unset($aval[$i][6]);
+    unset($aval[$i][7]);
+    unset($aval[$i][23]);
 }
 
-$filename = 'Reporte de cartas ' . $current_timestamp . '.xlsx';
+$filename = 'Reporte de cartas.xlsx';
 
 $xlsx = Shuchkin\SimpleXLSXGen::fromArray($cartas);
 $xlsx->downloadAs($filename);
