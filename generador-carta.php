2a631f69b9de5f105ad4d1c3302707c53a09d746
Diego Martinez <115640081+Microyuc-iyem@users.noreply.github.com>
2024-03-12 13:13:36 -0600

    Update generador-carta.php

--- generador-carta.php
+++ generador-carta.php
@@ -223,13 +223,16 @@ $formatter = new IntlDateFormatter(
 );
 
 // Formatear la fecha con el nombre del mes en español
-$fecha_visita_formatted = $formatter->format($fecha_visita);
+$fecha_visita_formatted = $formatter->format(new DateTime($carta['fecha_visita']));
 
 // Reemplazar el día y el año en la cadena formateada
-$fecha_visita_formatted = $fecha_visita->format('l, j \d\e F \d\e Y');
+$fecha_visita_formatted = preg_replace('/([0-9]{1,2}\s+de)\s+([A-Za-z]+)\s+de\s+([0-9]{4})/', '$1 de $2 del $3', $fecha_visita_formatted);
 
-// Insertar la fecha formateada en el documento Word
-      $templateProcessor->setValue('fecha_visita', "Mérida, Yucatán, México, $fecha_visita_formatted"); 
+// Eliminar la hora de la fecha formateada
+$fecha_visita_formatted = explode(' ', $fecha_visita_formatted)[0];
+
+        // Insertar la fecha formateada en el documento Word
+        $templateProcessor->setValue('fecha_visita', "Mérida, Yucatán, México, $fecha_visita_formatted");
         $templateProcessor->setValue('numero_expediente', $carta['numero_expediente']);
         $templateProcessor->setValue('nombre_cliente', $carta['nombre_cliente']);
         $templateProcessor->setValue('calle', $carta['calle']);
